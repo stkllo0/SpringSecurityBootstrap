c569099237f83b54eb3618323641a666121754f2
stkllo0 <106972900+stkllo0@users.noreply.github.com>
2023-03-13 12:47:03 +0300

    Update AdminController.java

--- PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,18 +1,14 @@
 package ru.kata.spring.boot_security.demo.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
 import ru.kata.spring.boot_security.demo.repository.UserRepository;
 import ru.kata.spring.boot_security.demo.service.RoleServiceImpl;
-import ru.kata.spring.boot_security.demo.service.UserService;
 import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
 import java.security.Principal;
-import java.util.List;
+
 
 @Controller
 @RequestMapping("/admin")
@@ -36,18 +32,18 @@ public class AdminController {
         model.addAttribute("userName", userService.findByUsername(principal.getName()));
         model.addAttribute("users", userService.getAllUsers());
         model.addAttribute("roles", roleService.getAllRoles());
-        model.addAttribute("newUser", new User());
-        model.addAttribute("authUser", user);
+        model.addAttribute("user", new User());
+        model.addAttribute("thisUser", user);
         return "allUsers";
     }
 
 
-    @GetMapping("/add")
-    public String getCreationUserForm(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "redirect:/admin";
-    }
+//    @GetMapping("/add")
+//    public String getCreationUserForm(Model model) {
+//        User user = new User();
+//        model.addAttribute("user", user);
+//        return "redirect:/admin";
+//    }
 
     @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute("user") User user) {
@@ -67,7 +63,7 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-    @PatchMapping("/user/{id}")
+    @PatchMapping("/update/{id}")
     public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") int id) {
         userService.updateUser(id, user);
         return "redirect:/admin";
